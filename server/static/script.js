5710fa00cecf61749deb86ac6022da6ee365ac1b
William Stevens <contact@wastevensv.com>
2016-12-03 18:33:15 -0500

    Fixes emacs warnings.

--- server/static/script.js
+++ server/static/script.js
@@ -1,78 +1,79 @@
-'use strict'
+'use strict';
 
-const URL = window.location.origin
-const dashboard = document.querySelector('.dashboard-container')
-let updatedAt = 0
+const URL = window.location.origin;
+const dashboard = document.querySelector('.dashboard-container');
+let updatedAt = 0;
 
 let newTextBoard = board => {
-  let el = document.createElement('p')
-  el.appendChild(document.createTextNode(board.content))
-  return el
-}
+    let el = document.createElement('p');
+    el.appendChild(document.createTextNode(board.content));
+    return el;
+};
 
 let newImageBoard = board => {
-  let el = document.createElement('img')
-  el.src = board.content
-  return el
-}
+    let el = document.createElement('img');
+    el.src = board.content;
+    return el;
+};
 
 let createBoard = board => {
-  let el
+    let el;
   if (board.type == 'text') {
-    el = newTextBoard(board)
+      el = newTextBoard(board);
   } else if (board.type == 'image-url') {
-    el = newImageBoard(board)
+      el = newImageBoard(board);
   }
-  el.className += board.size
-  let style = `bottom: ${board.location.y}%;`
-  style += `right: ${board.location.x}%;`
-  el.setAttribute('style', style)
-  dashboard.appendChild(el)
-}
+    el.className += board.size;
+    let style = `bottom: ${board.location.y}%;`;
+    style += `right: ${board.location.x}%;`;
+    el.setAttribute('style', style);
+    dashboard.appendChild(el);
+};
 
 let updateView = () => {
   fetch(`${URL}/dashboard.json`).then(res => {
     if (!res.ok) {
-      console.log('Network response was not ok.')
+        console.log('Network response was not ok.');
+        return null;
     } else {
-      return res.json()
+        return res.json();
     }
-  }).then(processJSON)
-}
+  }).then(processJSON);
+};
 
 let processJSON = data => {
   if (data && data.boards) {
     let timestamps = data.boards.map(board => {
-      return board.timestamp
-    })
-    let mostRecentTimestamp = Math.max.apply(null, timestamps)
+        return board.timestamp;
+    });
+      let mostRecentTimestamp = Math.max.apply(null, timestamps);
     if (mostRecentTimestamp > updatedAt) {
-      dashboard.innerHTML = ''
-      data.boards.map(createBoard)
-      data.boards.map(board => {console.log(board)})
-      updatedAt = mostRecentTimestamp
+        dashboard.innerHTML = '';
+        data.boards.map(createBoard);
+        data.boards.map(board => {console.log(board);});
+        updatedAt = mostRecentTimestamp;
     }
   }
-}
+};
 
 // form logic
 let clickHandler = event => {
-  let input = document.querySelector('#post-input')
-  input.select()
-  let inputContainer = document.querySelector('#input-container')
-  inputContainer.className = ''
-  inputContainer.setAttribute('style', `left: ${event.clientX}px; top: ${event.clientY}px;`)
-}
+    let input = document.querySelector('#post-input');
+    input.select();
+    let inputContainer = document.querySelector('#input-container');
+    inputContainer.className = '';
+    inputContainer.setAttribute('style', `left: ${event.clientX}px; top: ${event.clientY}px;`);
+};
 
 let submit = size => {
-  let type = 'text' // TODO: don't hardcode this
+  let type = 'text'; // TODO: don't hardcode this
   let params = {
     size: size,
     content: document.querySelector('#post-input').value,
     pos_x: document.querySelector('#x-pos'),
     pos_y: document.querySelector('#y-pos'),
     pos_z: 0
-  }
+  };
   fetch(`${URL}/post/${type}`, {
           headers: {
             'Accept': 'application/json',
@@ -81,6 +82,6 @@ let submit = size => {
           method: "POST",
           body: JSON.stringify(params)
   }).then(res => {
-    updateView()
-  })
+      updateView();
+  });
 }
