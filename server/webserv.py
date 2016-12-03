e0c6e2ce55e3f8413a7de3e49e2e4d93162a57b7
William Stevens <contact@wastevensv.com>
2016-12-02 22:41:07 -0500

    Moved static files. Serves dashboard and dashboard.json

--- server/webserv.py
+++ server/webserv.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, send_from_directory
 from json import dumps as jsonify
 from datetime import datetime as dt
 from server.database import session as db
@@ -9,6 +9,14 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "<h1 style='color:red'>Hello There!</h1>"
+
+@app.route("/dashboard")
+def dashboard():
+    return send_from_directory('static','index.html')
+
+@app.route("/dashboard.json")
+def dashboard_json():
+    return send_from_directory('static','example.json')
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
