2aa1675dba0619da1d8869c740c4beed8d881c9b
William Stevens <contact@wastevensv.com>
2016-12-02 23:53:31 -0500

    Lists the first 10 posts on dashboard.

--- server/models.py
+++ server/models.py
@@ -1,12 +1,14 @@
 from sqlalchemy import Column, Integer, String, Text, DateTime
+from sqlalchemy.sql import func
 
-from .database import Base
+from server.database import Base
 
 class Post(Base):
     __tablename__ = 'posts'
     id = Column(Integer, primary_key=True)
     sender = Column(String(64))
-    timestamp = Column(DateTime)
+    timestamp = Column(DateTime, server_default=func.now())
+    content_type = Column(String(16))
     content = Column(Text)
     size = Column(Integer)
     pos_x = Column(Integer)
--- server/webserv.py
+++ server/webserv.py
@@ -1,10 +1,25 @@
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, send_from_directory, redirect
+from sqlalchemy import desc
 from json import dumps as jsonify
 from datetime import datetime as dt
 from server.database import session as db
 import server.models as models
 
 app = Flask(__name__)
+
+def post_to_dict(post):
+    return {
+            'type':post.content_type,
+            'sender':post.sender,
+            'timestamp':post.timestamp.timestamp(),
+            'content':post.content,
+            'size':post.size,
+            'location': {
+                'x':post.pos_x,
+                'y':post.pos_y,
+                'z':post.pos_z
+            }
+        }
 
 @app.route("/")
 def hello():
@@ -14,9 +29,20 @@ def hello():
 def dashboard():
     return send_from_directory('static','index.html')
 
+@app.route("/post/text/<text>")
+def post_text(text):
+    p = models.Post(content=text, content_type="text")
+    db.add(p)
+    db.commit()
+    return redirect('dashboard')
+
 @app.route("/dashboard.json")
 def dashboard_json():
-    return send_from_directory('static','example.json')
+    posts = models.Post.query.order_by(desc(models.Post.timestamp)).limit(10).all()
+    resp = []
+    for post in posts:
+        resp.append(post_to_dict(post))
+    return jsonify({"boards":resp})
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
